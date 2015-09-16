3f38f3a5a7d59c9ec46084269fb9d4dc3fd7e648
Tero Marttila <tero.marttila@aalto.fi>
2015-09-16 21:36:35 +0300

    skydns2: register container PTR records

--- skydns2/skydns2.go
+++ skydns2/skydns2.go
@@ -1,10 +1,12 @@
 package skydns2
 
 import (
+	"fmt"
 	"encoding/json"
 	"log"
-	"net/url"
+	"net"
 	"strings"
+	"net/url"
 
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/gliderlabs/registrator/bridge"
@@ -48,25 +50,51 @@ func (r *Skydns2Adapter) Ping() error {
 	return nil
 }
 
-func (r *Skydns2Adapter) Register(service *bridge.Service) error {
-	record, err := json.Marshal(Service{r.serviceHost(service), service.Port})
+func (r *Skydns2Adapter) register(path string, service Service, ttl int) error {
+	value, err := json.Marshal(service)
 	if err != nil {
-		log.Println("skydns2: failed to marshal service:", err)
-		return err
+		log.Println("skydns2: failed to marshal service", path, ":", err)
 	}
 
-	_, err = r.client.Set(r.servicePath(service), string(record), uint64(service.TTL))
+	_, err = r.client.Set(path, string(value), uint64(ttl))
 	if err != nil {
-		log.Println("skydns2: failed to register service:", err)
+		log.Println("skydns2: failed to register service", path, ":", err)
+	}
+
+	log.Println("skydns2: register service", path, ":", string(value))
+
+	return nil
+}
+
+func (r *Skydns2Adapter) deregister(path string) error {
+	_, err := r.client.Delete(path, false)
+	if err != nil {
+		log.Println("skydns2: failed to deregister service", path, ":", err)
 	}
 	return err
 }
 
-func (r *Skydns2Adapter) Deregister(service *bridge.Service) error {
-	_, err := r.client.Delete(r.servicePath(service), false)
-	if err != nil {
-		log.Println("skydns2: failed to register service:", err)
+func (r *Skydns2Adapter) Register(service *bridge.Service) error {
+	if reversePath := r.reversePath(service); reversePath != "" {
+		if err := r.register(reversePath, Service{Host: r.containerDomain(service)}, service.TTL); err != nil {
+			return err
+		}
 	}
+	if containerPath := r.containerPath(service); containerPath != "" {
+		if err := r.register(containerPath, Service{Host: service.IP}, service.TTL); err != nil {
+			return err
+		}
+	}
+	if servicePath := r.servicePath(service); servicePath != "" {
+		if err := r.register(servicePath, Service{Host: r.containerDomain(service), Port: service.Port}, service.TTL); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *Skydns2Adapter) Deregister(service *bridge.Service) error {
+	err := r.deregister(r.servicePath(service))
 	return err
 }
 
@@ -82,20 +110,34 @@ func (r *Skydns2Adapter) serviceDomain(service *bridge.Service) string {
 	return domainJoin(service.Container.Hostname, service.Host.Hostname, "_" + service.Origin.ExposedPort, "_" + service.Origin.PortType, service.Name, r.domain)
 }
 
-func (r *Skydns2Adapter) serviceHost(service *bridge.Service) string {
-	if service.Port == 0 {
-		return service.IP
-	} else {
-		return r.containerDomain(service)
+func (r *Skydns2Adapter) reversePath(service *bridge.Service) string {
+	if service.Port != 0 {
+		return ""
 	}
+
+	ip := net.ParseIP(service.IP)
+	domain := reverseDomain(ip)
+	if domain == "" {
+		return ""
+	}
+
+	return domainPath(domain)
+}
+
+func (r *Skydns2Adapter) containerPath(service *bridge.Service) string {
+	if service.Port != 0 {
+		return ""
+	}
+
+	return domainPath(r.containerDomain(service))
 }
 
 func (r *Skydns2Adapter) servicePath(service *bridge.Service) string {
 	if service.Port == 0 {
-		return domainPath(r.containerDomain(service))
-	} else {
-		return domainPath(r.serviceDomain(service))
+		return ""
 	}
+
+	return domainPath(r.serviceDomain(service))
 }
 
 func domainJoin(components... string) string {
@@ -110,4 +152,10 @@ func domainPath(domain string) string {
 	return "/skydns/" + strings.Join(components, "/")
 }
 
-
+func reverseDomain(ip net.IP) string {
+	if ip4 := ip.To4(); ip4 != nil {
+		return fmt.Sprintf("%d.%d.%d.%d.in-addr.arpa", ip4[3], ip4[2], ip4[1], ip4[0])
+	} else {
+		return ""
+	}
+}
